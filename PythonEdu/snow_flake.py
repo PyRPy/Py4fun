8aca418812b39de9096cc469f334a77ccccaf657
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-03-13 16:59:52 -0500

    how to draw a snowflake in python

---
+++ PythonEdu/snow_flake.py
@@ -0,0 +1,10 @@
+import turtle
+flake = turtle.Turtle()
+
+for i in range(10):
+    for j in range(2):
+        flake.forward(100)
+        flake.right(60)
+        flake.forward(100)
+        flake.right(120)
+    flake.right(36)
