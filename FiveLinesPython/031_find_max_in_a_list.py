959317bc8bb2aa2dd15fdb4d8d47d95bcef714c4
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-05-21 19:28:09 -0400

    find index for a specified value in a list

---
+++ FiveLinesPython/031_find_max_in_a_list.py
@@ -0,0 +1,13 @@
+Python 3.8.5 (default, Sep  3 2020, 21:29:08) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # find the max number in a list
+>>> myList = [1, 3, 2, 5, 9, 7]
+>>> max(myList)
+9
+>>> # find the index or where it is ?
+>>> myList.index(9)
+4
+>>> # check it again
+>>> myList[4] == 9
+True
+>>> 
