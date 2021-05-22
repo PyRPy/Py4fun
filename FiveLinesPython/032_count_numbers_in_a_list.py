1394cdf0809f6ea2426757f4760720e861485de1
PyRPy <44626163+PyRPy@users.noreply.github.com>
2021-05-22 11:19:17 -0400

    list method for count()

---
+++ FiveLinesPython/032_count_numbers_in_a_list.py
@@ -0,0 +1,11 @@
+Python 3.8.5 (default, Sep  3 2020, 21:29:08) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # count how many times a number in a list
+>>> myList = [2, 3, 5, 2, 6, 2, 6]
+>>> myList.count(2)
+3
+>>> myList.count(5)
+1
+>>> myList.count(6)
+2
+>>> 
