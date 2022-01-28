ecb596a40c37301015ce4595318b5f8335fb9a56
PyRPy <44626163+PyRPy@users.noreply.github.com>
2022-01-28 17:35:37 -0500

    Update 2: text for each gua, use dict to find text

---
+++ iChingSampling/gua64.py
@@ -0,0 +1,4 @@
+# this file store the map from 'yao' to 'gua number'
+gua_dict = {str([1, 1, 1, 1, 1, 1]): "01.txt",
+            str([0, 0, 0, 0, 0, 0]): "02.txt",
+            str([1, 0, 0, 0, 1, 1]): "03.txt"}
